7c9b84ef716b3568a6760599847c5bde0d9406b0
ShreyashKshirsagar17 <shreyashk1730@gmail.com>
2024-09-25 13:16:01 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,26 @@
+#x=True
+#y=str(x)
+#print(y)
+
+
+#print(4/2)
+
+# x=5.00001
+# l=5.0000
+# print(x==l)
+
+# x=2
+# y=3
+# print(x,y)
+
+# x='hello world'
+# print(x[0:9:2])
+# print(x[3:10:3])
+
+# x='abcdefgh'
+# print(x[-3:-6:-1])
+# print(x[-1:-6:-2])
+# print(x[-1:-6:-4])
+# print(x[-6:-9:-1])
+
+print(4%8)
