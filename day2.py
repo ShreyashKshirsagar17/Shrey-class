945f1a53143c7c36ec39cbb11a6a5b5273b02b07
ShreyashKshirsagar17 <shreyashk1730@gmail.com>
2024-09-26 15:51:34 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,34 @@
+# x=input('Enter your name ')
+# age=int(input('Enter your age '))
+# if age>=18:
+#     print('you are older')
+# else:
+#     print('your are yo
+   
+
+
+# print('fro Even and odd statment ')
+
+# x=int(input('Enetr a no :'))
+# if x%2==0:
+#     print("no is even")
+# else:
+#     print('no is odd')
+
+
+# print('for positive or negative')
+# x=int(input('Enter a no :'))
+
+# if x>=0:
+#     print('on is positive ')
+# else:
+#     print('on is negative')
+
+
+x=input('enter a name :')
+y=(x[::-1])
+print(y)
+if y==x:
+    print('name is  palindrome')
+else:
+    print('name is not palindrome')
