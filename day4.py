f21e0ae58b113ef555ed8b6b693e731978ae5a88
ShreyashKshirsagar17 <shreyashk1730@gmail.com>
2024-09-29 02:29:52 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,17 @@
+# i=1
+# while i<=100 :
+#     if i%2==0:  
+#         print(i)
+#     i+=1
+
+
+# i=1
+# while i<=100:
+#     if i%2!=0:
+#         print(i)
+#     i+=1
+
+for i in range(4):
+    for j in range(4):
+        print('*' , end=' ')
+    print()
