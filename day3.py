f5cb31deba4a6332b4a909829e5537879b411554
ShreyashKshirsagar17 <shreyashk1730@gmail.com>
2024-09-28 13:28:45 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,42 @@
+# name=input("enter your name ")
+# age=int(input("enetr your age"))
+# if age>=18:
+#     print("eligible ")
+# else:
+#     print("not eligible")
+
+
+# print('for elif condition ')
+
+
+# ab=1710
+# x=int(input('Enter your age :'))
+# if x>=20 and x<=30:
+#     print('your creadited 10000 and your current balance is ')
+#     print(ab+10000)
+# elif x>=35 and x<=45:
+#     print('your creadited 15000 and your current balance is ' )
+#     print(ab+15000)
+# elif x>=50 and x<=60:
+#     print('your creadited 20000 and your current balance is ' )
+#     print(ab+20000)
+# else:
+#     print(' your not eligible for this ')
+
+
+# x=input(' enter a letter :')
+# y=x.lower()
+# if y=='a' or y=='e' or y=='i' or y=='o' or y=='u':
+#     print('letter is V')
+# else:
+#     print('letter is letter ')
+
+
+
+
+year=int(input('Enter a year :'))
+if(year%4==0 and year%100!=0)or (year%400==0):
+    print('This is leap yera ')
+else:
+    print('this is not a leap year ')
+
