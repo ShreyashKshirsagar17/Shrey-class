e13b3d3e6caff77cb31b0fcb49e07addace1f5ed
ShreyashKshirsagar17 <shreyashk1730@gmail.com>
2024-09-29 21:39:09 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,32 @@
+
+# x=[1,2,3,4,5,6,7,8,9,10]
+# even=[]
+# odd=[]
+# for i in x:
+#     if i%2==0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+
+# print(even)
+# print(odd)
+
+
+# x=[1,-2,-3,4,-5,6,-7,8,-9,11,2,-3,1,22,-33,]
+# positive=[]
+# negative=[]
+# for i in x:
+#     if i>0:
+#         positive.append(i)
+#     else:
+#         negative.append(i)
+# print(positive)
+# print(negative)
+
+
+x=[1,2,3,5,2,10,15,33,25,40,45,23]
+y=[]
+for i in x:
+    if i%5==0:
+        y.append(i)
+print(y)
